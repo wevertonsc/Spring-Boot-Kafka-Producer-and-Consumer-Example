ae0f91dc9c2ae207c54ac8a3c1851f3d6e9825f8
Weverton de Souza Castanho <weverton.castanho@Air-de-Weverton.station>
2022-02-13 19:32:31 +0000

    update docker kafka port

--- src/main/java/com/netsurfingzone/constant/ApplicationConstant.java
+++ src/main/java/com/netsurfingzone/constant/ApplicationConstant.java
@@ -1,7 +1,7 @@
 package com.netsurfingzone.constant;
 
 public class ApplicationConstant {
-	public static final String KAFKA_LOCAL_SERVER_CONFIG = "localhost:9092";
+	public static final String KAFKA_LOCAL_SERVER_CONFIG = "localhost:29092";
 	public static final String GROUP_ID_STRING = "group-id-string-1";
 	public static final String TOPIC_NAME = "performance-test";
 	public static final String KAFKA_LISTENER_CONTAINER_FACTORY = "kafkaListenerContainerFactory";
